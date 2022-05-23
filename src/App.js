6973891c4cbdffd4dca8290e466abb553735d23c
gloriyo <gloriajyoon@gmail.com>
2022-05-23 15:42:34 -0700

    react app

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
+import { Nav } from 'react-bootstrap'
+
 import logo from './logo.svg';
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
+      <MainNav />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <h1> Gloria Yoon</h1>
+        {/* <p>
           Edit <code>src/App.js</code> and save to reload.
-        </p>
+        </p> */}
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -20,6 +25,31 @@ function App() {
       </header>
     </div>
   );
+}
+
+function MainNav() {
+  return(
+    <Nav activeKey="/home">
+      <Nav.Item>
+        <Nav.Link href="/home">Home</Nav.Link>
+      </Nav.Item>
+      <div class="vr"></div>
+      <Nav.Item>
+        <Nav.Link eventKey="link-1">Projects</Nav.Link>
+      </Nav.Item>
+      <div class="vr"></div>
+      <Nav.Item>
+        <Nav.Link eventKey="link-2">More</Nav.Link>
+      </Nav.Item>
+      <div class="vr"></div>
+      <Nav.Item>
+        <Nav.Link eventKey="disabled" disabled>
+          Disabled
+        </Nav.Link>
+      </Nav.Item>
+    </Nav>
+  );
+
 }
 
 export default App;
